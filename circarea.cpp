b19ee10df06d61768a26cf6ba4ae722d7e5e85c1
WePlayFleX <alex.kamai@ya.ru>
2021-11-29 23:58:56 +0300

    Добавьте файлы проекта.

---
+++ circarea.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>			
+
+using namespace std;
+
+int main()
+{	
+	float rad;						
+	const float PI = 3.14159F;		
+	cout << "Enter the radius of the circle: ";
+	cin >> rad;
+	float area = PI * rad * rad;
+	cout << "The area of the circle is " << area << endl;
+
+	system("pause");
+	return 0;
+}
