d1efcfe9406da1a404a56f06a66d38c85b3db905
WePlayFleX <alex.kamai@ya.ru>
2022-01-18 23:27:53 +0300

    V1

--- circarea.cpp
+++ circarea.cpp
@@ -1,16 +1,40 @@
-﻿#include <iostream>			
+﻿//#include <iostream>			
+//
+//using namespace std;
+//
+//int main()
+//{	
+//	float rad;						
+//	const float PI = 3.14159F;		
+//	cout << "Enter the radius of the circle: ";
+//	cin >> rad;
+//	float area = PI * rad * rad;
+//	cout << "The area of the circle is " << area << endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
+#include <iostream>			
 
 using namespace std;
 
+float circarea(float radius);
+
 int main()
-{	
-	float rad;						
-	const float PI = 3.14159F;		
+{
+	double rad;
 	cout << "Enter the radius of the circle: ";
 	cin >> rad;
-	float area = PI * rad * rad;
-	cout << "The area of the circle is " << area << endl;
+	cout << "The area of the circle is " << circarea(rad) << endl;
 
 	system("pause");
 	return 0;
+}
+
+float circarea(float r)
+{
+	const float PI = 3.14159F;
+
+	return r * r * PI;
 }
